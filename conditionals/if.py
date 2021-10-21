62ee3b4b960a78317df0ed9ea0368c86e7c643c7
Dustin-Stewart <dustin.nola@gmail.com>
2021-10-21 13:21:57 -0500

    conditionals module copmlete

--- conditionals/if.py
+++ conditionals/if.py
@@ -6,3 +6,39 @@ if (x==2):
 elif x>=y:print('as a lambda\nx >= y')
 
 else: print('idk')
+
+print('password practice')
+pwd = 'pwd'
+count = 0
+
+while count <=4:
+    tries = 5-count
+    count+=1
+    inp = input('\nenter the pwd\nremaining tries: '+str(tries)+'\n')
+
+    if inp==pwd:
+        print('thank you')
+        exit(0)
+
+    elif tries>2:
+        print('try again')
+
+    elif tries == 2:
+        print('hint: the pwd is: \'pwd\'')
+
+    else:
+        print('try again later')
+
+
+print('\nnesting\n')
+
+x=y=3
+z=4
+
+if x==y:
+    if x!=z:
+        if y!=z:
+            if z>x:
+                print('x == y')
+else:
+    ('somethings wrong')
