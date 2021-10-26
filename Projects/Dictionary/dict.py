1ccb2c744a3e60af6398776df781d15f128a033d
Dustin-Stewart <dustin.nola@gmail.com>
2021-10-26 15:30:57 -0500

    provided case for word not found and provided an exit code

--- Projects/Dictionary/dict.py
+++ Projects/Dictionary/dict.py
@@ -3,12 +3,20 @@ dictionary=PyDictionary()
 
 def getDef(x):
     while True:
-        define = dictionary.meaning(x)
-        print('\n'+x+':')
-        for k,v in define.items():
-            i=1
-            print('\n',k,': ')
-            for item in v:
-                print (i,') ',item)
-                i+=1
-        x = input('\nEnter your search term here: ')
+        if x==('x!'):
+            print('Thanks for running me!\n')
+            exit(0)
+
+        if (dictionary.meaning(x,True)):
+            define = dictionary.meaning(x)
+            print('\n'+x+':')
+            for k,v in define.items():
+                i=1
+                print('\n',k,': ')
+                for item in v:
+                    print (i,') ',item)
+                    i+=1
+            x = input('\nEnter your search term here, or enter \'x!\' to exit: ')
+        else:
+            print('\n',x,' is not a word')
+            x = input('\nEnter your search term here, or enter \'x!\' to exit: ')
